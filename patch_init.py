d17ca7bc8af9a360fc40ec71e12b058730febb96
5akur0 <wxiangyu231@gmail.com>
2026-05-29 19:27:04 +0800

    chore: remove temporary build scripts

--- patch_init.py
+++
@@ -1,8 +0,0 @@
-import re
-with open('script.js', 'r') as f:
-    js = f.read()
-
-js = js.replace('  renderUI();\n  requestAnimationFrame(tick);', '  if (!loadAutoSave()) {\n    setPhase("choose");\n  }\n  renderUI();\n  requestAnimationFrame(tick);')
-
-with open('script.js', 'w') as f:
-    f.write(js)
